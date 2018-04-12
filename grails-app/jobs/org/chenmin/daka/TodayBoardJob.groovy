7e394bab3632f2d142f91cb565c736d0b3856c9d
chenmin <admin@chenmin.org>
2018-04-12 17:38:42 +0800

    today 每日生成

--- grails-app/jobs/org/chenmin/daka/TodayBoardJob.groovy
+++ grails-app/jobs/org/chenmin/daka/TodayBoardJob.groovy
@@ -1,6 +1,7 @@
 package org.chenmin.daka
 
 import grails.converters.JSON
+import groovy.sql.Sql
 
 class TodayBoardJob {
     def a = '''
@@ -16,9 +17,13 @@ cronExpression: "s m h D M W Y"
 '''
     static triggers = {
         cron name: 'todayTrigger', cronExpression: "0 * * * * ?"
+        //这个是真正的每日六点执行
+        //cron name: 'todayTrigger', cronExpression: "0 0 6 * * ?"
     }
     def group = "todayGroup"
     def description = "每日早上6：00开始执行产生新的数据"
+
+    def dataSource
 
     def execute() {
         // execute job
@@ -26,26 +31,41 @@ cronExpression: "s m h D M W Y"
         def hasToday = TodayBoard.findByYmd(DateTool.today())
         if(hasToday!=null){
             println hasToday as JSON
-            println DateTool.time()+" has exsit!"
+            println DateTool.today()+" "+  DateTool.time() +" has exsit!"
             return
         }
         println DateTool.today()+" "+ DateTool.time()+" TodayBoard init!"
-        //增加测试的今日看板
+        int spaid = 0
+        int currentCount = 0
+        def sql = new Sql(dataSource);
+        String strSql = "select sum(paid) spaid from daka_clock_user t ";
+        sql.eachRow(strSql) {
+            spaid = it.spaid
+        }
+        strSql = "select count(paid) counts from daka_clock_user t where t.paid>0 ";
+        sql.eachRow(strSql) {
+            currentCount = it.counts
+        }
+        //增加初始化的今日看板
         def earlyStar = ClockUser.get(1)
         def staminaStar = ClockUser.get(2)
         def today = new TodayBoard()
         today.ymd=DateTool.today()
-        today.currentTotalMoney=2360000
-        today.currentParticipateCount=231
-        today.hitClock=170
-        today.notHitClock=61
+        //当前挑战金
+        today.currentTotalMoney=spaid
+        //当前挑战人数
+        today.currentParticipateCount=currentCount
+        //当前已经打卡人数
+        today.hitClock=0
+        //当前已经未打卡人数
+        today.notHitClock=today.currentParticipateCount
         today.earlyStar = earlyStar
         today.earlyTime = today.earlyStar.todayTime
         today.staminaStar = staminaStar
         today.staminaCount = today.staminaStar.staminaCount
-        today.hitMoney=2300000
-        today.notHitMoney=60000
-        today.thousandRewardMoney=531
+        today.hitMoney=0
+        today.notHitMoney=today.currentTotalMoney
+        today.thousandRewardMoney=-1
         today.save()
         println DateTool.today()+" "+ DateTool.time()+" TodayBoard has created!"
 
