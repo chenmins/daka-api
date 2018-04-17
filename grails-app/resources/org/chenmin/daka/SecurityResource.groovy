1a3b5f60e8076b9fd1e495c24688926854cd95e1
chenmin <admin@chenmin.org>
2018-04-18 00:55:29 +0800

    下单挑战金测试

--- grails-app/resources/org/chenmin/daka/SecurityResource.groovy
+++ grails-app/resources/org/chenmin/daka/SecurityResource.groovy
@@ -6,6 +6,10 @@ import io.swagger.annotations.Api
 import io.swagger.annotations.ApiOperation
 import io.swagger.annotations.ApiParam
 import org.apache.http.client.HttpClient
+import weixin.popular.api.PayMchAPI
+import weixin.popular.bean.paymch.Unifiedorder
+import weixin.popular.bean.paymch.UnifiedorderResult
+import weixin.popular.util.PayUtil
 
 import javax.ws.rs.GET
 import javax.ws.rs.Path
@@ -43,6 +47,48 @@ class SecurityResource {
         return json
     }
 
+    //下单
+    @GET
+    @Path('/order/{openid}/{cash}')
+    @ApiOperation(value = "下单挑战金测试", notes = "仅供测试")
+    @Produces(MediaType.APPLICATION_JSON)
+    String order(@ApiParam(required = true, value = "微信个人ID")
+               @PathParam("openid")
+                       String openid,
+               @ApiParam(required = true, value = "充值金额（单位分）")
+               @PathParam("cash")
+                       int cash) {
+        String url = "http://wx.bdh114.com/default/PayMchNotify"
+        String appid = "wxbd7ee929512fd71f"
+        String mch_id = "1490841962"
+        String key = "J8HTUYWLYIPLJLELU3D4GPLNO7FYNFH2"
+        String wx_openid = params.id
+        String fee = ""+cash
+        Unifiedorder unifiedorder = new Unifiedorder();
+        unifiedorder.setAppid(appid);
+        unifiedorder.setMch_id(mch_id);
+        unifiedorder.setNonce_str(UUID.randomUUID().toString().toString().replace("-", ""));
+
+        unifiedorder.setOpenid(wx_openid);
+        unifiedorder.setBody("商品信息");
+        unifiedorder.setOut_trade_no(UUID.randomUUID().toString().toString().replace("-", ""));
+        unifiedorder.setTotal_fee(fee);//单位分
+        unifiedorder.setSpbill_create_ip(request.getRemoteAddr());//IP
+        unifiedorder.setNotify_url(url);
+        unifiedorder.setTrade_type("JSAPI");//JSAPI，NATIVE，APP，WAP
+        //统一下单，生成预支付订单
+        UnifiedorderResult unifiedorderResult = PayMchAPI.payUnifiedorder(unifiedorder,key);
+        println "unifiedorderResult:"+params.id
+        println unifiedorder as JSON
+        println unifiedorderResult as JSON
+        //@since 2.8.5  API返回数据签名验证
+        if(unifiedorderResult.getSign_status() !=null && unifiedorderResult.getSign_status()){
+            String json = PayUtil.generateMchPayJsRequestJson(unifiedorderResult.getPrepay_id(), appid, key);
+
+            return json
+        }
+        return unifiedorderResult as JSON
+    }
     //支付测试
     @GET
     @Path('/pay/{openid}/{cash}')
