60ec7cc0c8783de577a980ac90c5e4337bb31296
Chenmin <cnlll@qq.com>
2018-04-17 19:42:14 +0800

    微信支付相关

---
+++ src/test/groovy/org/chenmin/daka/PayControllerSpec.groovy
@@ -0,0 +1,18 @@
+package org.chenmin.daka
+
+import grails.testing.web.controllers.ControllerUnitTest
+import spock.lang.Specification
+
+class PayControllerSpec extends Specification implements ControllerUnitTest<PayController> {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
