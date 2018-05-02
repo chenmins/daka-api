a1ae35bed3af05af4c1d5fec13cdac7f32da5087
Chenmin <cnlll@qq.com>
2018-05-02 20:10:38 +0800

    sum is null

---
+++ grails-app/resources/org/chenmin/daka/TestRefund.java
@@ -0,0 +1,43 @@
+package org.chenmin.daka;
+
+import weixin.popular.api.PayMchAPI;
+import weixin.popular.bean.paymch.SecapiPayRefund;
+import weixin.popular.bean.paymch.SecapiPayRefundResult;
+import weixin.popular.client.LocalHttpClient;
+import weixin.popular.util.XMLConverUtil;
+
+public class TestRefund {
+    public static void main(String argc[]) {
+        String appid = "wxbd7ee929512fd71f";
+        String mch_id = "1490841962";
+        String mch_key = "J8HTUYWLYIPLJLELU3D4GPLNO7FYNFH2";
+//        商户订单号	out_trade_no
+        String out_trade_no ="b0427232270b446ba7eaca73eefb40f0" ;
+//        商户退款单号	out_refund_no
+        String out_refund_no= "TK" + System.currentTimeMillis();
+//        订单金额	total_fee
+        Integer total_fee  = 100;
+//        退款金额	refund_fee
+        Integer refund_fee = 100;
+
+        String keyStoreFilePath = "D:\\wx\\apiclient_cert.p12";
+        LocalHttpClient.initMchKeyStore(mch_id, keyStoreFilePath);
+        SecapiPayRefund secapiPayRefund = new SecapiPayRefund();
+        secapiPayRefund.setNonce_str("NS" + System.currentTimeMillis());
+        secapiPayRefund.setAppid(appid);
+//        secapiPayRefund.setNotify_url("");
+        secapiPayRefund.setMch_id(mch_id);
+        //TODO 设置secapiPayRefund
+//
+        secapiPayRefund.setOut_trade_no(out_trade_no);
+        secapiPayRefund.setOut_refund_no(out_refund_no);
+        secapiPayRefund.setTotal_fee(total_fee);
+        secapiPayRefund.setRefund_fee(refund_fee);
+//        退款原因	refund_desc
+//        secapiPayRefund.setrefund
+        SecapiPayRefundResult tr = PayMchAPI.secapiPayRefund(secapiPayRefund, mch_key);
+        System.out.println( XMLConverUtil.convertToXML(tr));
+        System.out.println("getReturn_code:"+tr.getReturn_code());
+        System.out.println("getReturn_msg:"+tr.getReturn_msg());
+    }
+}
---
+++ grails-app/resources/org/chenmin/daka/TestTransfer.java
@@ -0,0 +1,60 @@
+package org.chenmin.daka;
+
+import weixin.popular.api.PayMchAPI;
+import weixin.popular.bean.paymch.Transfers;
+import weixin.popular.bean.paymch.TransfersResult;
+import weixin.popular.client.LocalHttpClient;
+import weixin.popular.util.XMLConverUtil;
+
+public class TestTransfer {
+
+    public static void main(String argc[]){
+        String appid = "wxbd7ee929512fd71f";
+        String mch_id = "1490841962";
+        String mch_key= "J8HTUYWLYIPLJLELU3D4GPLNO7FYNFH2";
+        String partner_trade_no = "TX" + System.currentTimeMillis();
+        String openid= "oIvCJ5TS79FQC5XQjsZQfRhlL-R8";
+        String amount= "30";
+        String desc= "测试付款到个人零钱0.3元";
+        String keyStoreFilePath= "D:\\wx\\apiclient_cert.p12";
+
+        Transfers transfers = new Transfers();
+        // <mch_appid>wxe062425f740c30d8</mch_appid>
+        transfers.setMch_appid(appid);
+        // <mchid>10000098</mchid>
+        transfers.setMchid(mch_id);
+        // <nonce_str>3PG2J4ILTKCH16CQ2502SI8ZNMTM67VS</nonce_str>
+        transfers.setNonce_str("NS" + System.currentTimeMillis());
+        // <partner_trade_no>100000982014120919616</partner_trade_no>
+        transfers.setPartner_trade_no(partner_trade_no);
+        // <openid>ohO4Gt7wVPxIT1A9GjFaMYMiZY1s</openid>
+        transfers.setOpenid(openid);
+        // <check_name>OPTION_CHECK</check_name>
+        // NO_CHECK
+        transfers.setCheck_name("NO_CHECK");
+        // <re_user_name>张三</re_user_name>
+        // <amount>100</amount>
+        transfers.setAmount(amount);
+        // <desc>节日快乐!</desc>
+        transfers.setDesc(desc);
+        // <spbill_create_ip>10.2.3.10</spbill_create_ip>
+        transfers.setSpbill_create_ip("10.2.3.10");
+        // <sign>C97BDBACF37622775366F38B629F45E3</sign>
+        LocalHttpClient.initMchKeyStore(mch_id, keyStoreFilePath);
+        /**
+         * 企业付款
+         *
+         * @param transfers
+         *            transfers
+         * @param key
+         *            key
+         * @return TransfersResult
+         */
+        TransfersResult tr = PayMchAPI.mmpaymkttransfersPromotionTransfers(
+                transfers, mch_key);
+
+        System.out.println( XMLConverUtil.convertToXML(tr));
+        System.out.println("getErr_code:"+tr.getErr_code());
+        System.out.println("getErr_code_des:"+tr.getErr_code_des());
+    }
+}
