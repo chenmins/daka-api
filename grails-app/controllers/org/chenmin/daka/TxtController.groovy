2b28e135e986d029cf6999b6d11d773782b606f8
Chenmin <cnlll@qq.com>
2018-05-04 11:54:05 +0800

    TxtController

--- grails-app/controllers/org/chenmin/daka/TxtController.groovy
+++ grails-app/controllers/org/chenmin/daka/TxtController.groovy
@@ -2,7 +2,16 @@ package org.chenmin.daka
 
 class TxtController {
 
+    def text
+
+
+    def set(){
+        text = params.text
+    }
+
     def index() {
+        if(text)
+            render text
         render "1493cff504894e16d739d4a2e91adf32"
     }
 }
