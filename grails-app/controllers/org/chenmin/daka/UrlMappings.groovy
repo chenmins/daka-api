f07f7d31eb44b8a5007cc3afb215f16e0eedfbe8
chenmin <admin@chenmin.org>
2018-05-28 14:26:46 +0800

    增加映射txt二级目录

--- grails-app/controllers/org/chenmin/daka/UrlMappings.groovy
+++ grails-app/controllers/org/chenmin/daka/UrlMappings.groovy
@@ -10,6 +10,7 @@ class UrlMappings {
         }
         "/"(controller:"showRestApi")
         "/*.txt"(controller:"txt")
+        "/*/*.txt"(controller:"txt")
         "/real"(view:"/index")
         "500"(view:'/error')
         "404"(view:'/notFound')
