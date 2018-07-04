4d87ca6b50035e08b266d7f987696e7eb74bdeee
chenmin <admin@chenmin.org>
2018-07-04 12:12:45 +0800

    闪图API

---
+++ grails-app/resources/org/chenmin/daka/WxstResource.groovy
@@ -0,0 +1,37 @@
+package org.chenmin.daka
+
+import io.swagger.annotations.ApiOperation
+import io.swagger.annotations.ApiParam
+
+import javax.ws.rs.Consumes
+import javax.ws.rs.GET
+import javax.ws.rs.POST
+import javax.ws.rs.Path
+import javax.ws.rs.PathParam
+import javax.ws.rs.Produces
+import javax.ws.rs.core.MediaType
+
+@Path('/api/wxst')
+class WxstResource {
+
+    @GET
+    @Path('/get/{hashFile}')
+    @Produces(MediaType.APPLICATION_JSON)
+    @ApiOperation(value = "存储图片", notes = "")
+    String get(  @ApiParam(required = true, value = "微信图片id")
+                 @PathParam("hashFile")String hashFile) {
+        return WxImages.findByHashFileAndValid(hashFile,true)
+    }
+
+    @POST
+    @Path('/save')
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    @ApiOperation(value = "存储图片", notes = "")
+    WxImages save(WxImages wx){
+        wx.save(flush: true)
+        return WxImages.findByHashFile(wx.hashFile)
+    }
+
+
+}
